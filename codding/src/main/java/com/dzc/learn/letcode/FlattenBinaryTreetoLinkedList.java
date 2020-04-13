a6d3a9dbb7d6d6b274bfdc8c92dc4e0269f03ab1
DongZc <dongzc15247@hundsun.com>
2020-04-13 20:43:50 +0800

    add letCode test

---
+++ codding/src/main/java/com/dzc/learn/letcode/FlattenBinaryTreetoLinkedList.java
@@ -0,0 +1,34 @@
+package com.dzc.learn.letcode;
+
+
+/**
+ * Given a binary tree, flatten it to a linked list in-place.
+ *
+ * For example, given the following tree:
+ *
+ *     1
+ *    / \
+ *   2   5
+ *  / \   \
+ * 3   4   6
+ * The flattened tree should look like:
+ *
+ * 1
+ *  \
+ *   2
+ *    \
+ *     3
+ *      \
+ *       4
+ *        \
+ *         5
+ *          \
+ *           6
+ */
+public class FlattenBinaryTreetoLinkedList {
+
+    public void flatten(TreeNode root) {
+
+    }
+
+}
